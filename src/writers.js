fed79307359e6f9fd9434decd16d91e17b5e1622
Saeed <saeedabdulrazak95@gmail.com>
2022-03-16 16:57:44 -0700

    first commit

---
+++ src/writers.js
@@ -0,0 +1,45 @@
+const writers=[
+    {
+        id:1,
+        name: "Razak Saeed",
+        avatar: "audre_lorde",
+        email: 'saeedabdulrazak95@gmail.com',
+        phone: "0246966079",
+
+    },
+        {
+        id:2,
+        name: "Razak Saeed",
+        avatar: "belle_hooks",
+         email: 'saeedabdulrazak95@gmail.com',
+        phone: "0246966079",
+
+    },
+        {
+        id:3,
+        name: "Razak Saeed",
+        avatar: "maya_angelou",
+        email: 'saeedabdulrazak95@gmail.com',
+        phone: "0246966079",
+
+    },
+        {
+        id:4,
+        name: "Razak Saeed",
+        avatar: "james_baldwin",
+        email: 'saeedabdulrazak95@gmail.com',
+        phone: "0246966079",
+
+    },
+        {
+        id:5,
+        name: "Razak Saeed",
+        avatar: "du_bois",
+        email: "saeedabdulrazak95@gmail.com",
+        phone: "0246966079",
+
+    }
+]
+
+
+export default writers
